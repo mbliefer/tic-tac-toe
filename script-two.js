ebd7b704cf9edd7287833c89e72d0c7fa7a02c9d
mbliefer <mblief@gmail.com>
2024-01-12 18:43:28 -0600

    changes

--- script-two.js
+++ script-two.js
@@ -1,72 +1,129 @@
 const gameboard = (() => {
     // DOM
-    const reset = document.querySelector('.resetButton')
     const squares = document.querySelectorAll('.square');
 
-    // create Array for gameboard
+    let winner = null;
 
-    // display which player turn it is
+
+    // create Array for gameboard
+    let board = [];
+
+    const placeSymbol = (e) => {
+        if (e.target.textContent !== '') return
+
+        let index = e.target.dataset.position;
+        board[index] = gameplay.getPlayerTurnSymbol();
+
+        gameplay.pickSquare(e);
+        gameplay.displayPlayerTurn();
+    }
 
     // evaluate board, check for winner
+    const checkWinner = () => {
+        winArrs = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        winArrs.forEach((combo) => {
+            if (board[combo[0]]
+                && board[combo[0]] === board[combo[1]]
+                && board[combo[0]] === board[combo[2]]) {
+                    console.log('winner');
+                    winner = gameplay.getPlayerTurnName();
+                    console.log(winner + " wins");
+                }
+        });
+
+        return winner || (board.length < 9 ? null : 'Tie');
+    };
 
     // reset
+    const resetBoard = () => {
+        board = [];
+        squares.forEach((square) => {
+            square.textContent = "";
+        });
+    };
 
     // bind events
     squares.forEach((square) => {
-        square.addEventListener('click', gameplay.pickSquare)
+        square.addEventListener('click', placeSymbol)
     });
 
-    })();
+    return {
+        resetBoard,
+        checkWinner
+    }
 
-    const Player = (name, symbol) => {
-        const getName = () => name;
-        const getSymbol = () => symbol;
-        return {
-            getName,
-            getSymbol
-        };
+})();
+
+const Player = (name, symbol) => {
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    return {
+        getName,
+        getSymbol
+    };
+};
+
+const gameplay = (() => {
+    const playerX = Player('Player X', 'X');
+    const playerO = Player('Player O', 'O');
+    let playerTurn = playerX;
+    const getPlayerTurnSymbol = () => playerTurn.getSymbol();
+    const getPlayerTurnName = () => playerTurn.getName();
+
+    // DOM
+    const playerTurnText = document.querySelector('.displayTurnText')
+    const reset = document.querySelector('.resetButton')
+
+
+    const switchPlayerTurn = (player) => {
+        playerTurn = (playerTurn === playerX) ? playerO : playerX;
+    }
+
+    // display which player turn it is
+    const displayPlayerTurn = () => {
+        playerTurnText.textContent = getPlayerTurnName() + " it is your turn";
+    }
+
+    const pickSquare = (e) => {
+        e.target.textContent = getPlayerTurnSymbol();
+        gameboard.checkWinner();
+        switchPlayerTurn(playerTurn);
+        showWinner();
+    }
+
+    const showWinner = () => {
+        if (gameboard.checkWinner() !== null) {
+            playerTurnText.textContent = 'tie';
+        }
+    }
+
+    const resetPlayerTurnToX = () => {
+        playerTurn = playerX;
+        displayPlayerTurn();
     };
 
-    const gameplay = (() => {
-        const playerX = Player('Player X', 'X');
-        const playerO = Player('Player O', 'O');
-        let playerTurn = playerX;
+    // bind events
+    reset.addEventListener('click', () => {
+        gameboard.resetBoard();
+        resetPlayerTurnToX();
+    } );
 
 
-        // DOM
-        const squares = document.querySelectorAll('.square');
+    return {
+        pickSquare,
+        getPlayerTurnSymbol,
+        getPlayerTurnName,
+        displayPlayerTurn,
+    };
 
-        // player picks a square 
-        const pickSquare = (e) => {
-            target = e.target;
-            target.textContent = playerTurn.getSymbol();
-            switchPlayerTurn(playerTurn);
-        }
-
-        const switchPlayerTurn = (player) => {
-            if (player === playerX) {
-                playerTurn = playerO;
-            }
-            else { playerTurn = playerX; }
-        }
-
-        // reset board
-        const resetBoard = () => {
-
-        }
-
-
-
-
-        // bind events
-        // squares.forEach((square) => {
-        //     square.addEventListener('click', pickSquare)
-        // });
-
-
-
-        return {
-            pickSquare
-        };
-
-    })();
+})();
